e4a5053b312f9d4124ec8d53365b213156d16880
il_ch <zyhoneec@gmail.com>
2023-06-21 04:18:16 +0600

    added fixtures and png downloading

--- src/index.js
+++ src/index.js
@@ -3,13 +3,33 @@
 import axios from 'axios';
 import fs from 'fs/promises';
 import path from 'path';
+import cheerio from 'cheerio';
 
 const downloadPage = async (url, outputDir = process.cwd()) => {
   try {
-    const response = await axios.get(url);
+    const response = await axios.get(url, { responseType: 'arraybuffer' });
     const fileName = `${url.replace(/https?:\/\//i, '').replace(/[^a-zA-Z0-9]/g, '-')}.html`;
     const filePath = path.join(outputDir, fileName);
-    await fs.writeFile(filePath, response.data);
+    const $ = cheerio.load(response.data);
+
+    // Скачивание и изменение ссылок на изображения
+    const imagePromises = $('img').map(async (index, element) => {
+      const imageUrl = $(element).attr('src');
+      const imageFileName = `${imageUrl.replace(/https?:\/\//i, '').replace(/[^a-zA-Z0-9]/g, '-')}`;
+      const imageFilePath = path.join(outputDir, `${fileName.replace('.html', '_files')}`, imageFileName);
+
+      // Скачивание изображения
+      const imageResponse = await axios.get(imageUrl, { responseType: 'arraybuffer' });
+      await fs.writeFile(imageFilePath, imageResponse.data);
+
+      // Изменение ссылки на скачанное изображение
+      $(element).attr('src', `${fileName.replace('.html', '_files')}/${imageFileName}`);
+    }).get();
+
+    await Promise.all(imagePromises);
+
+    // Сохранение измененного HTML
+    await fs.writeFile(filePath, $.html());
     console.log(path.resolve(filePath));
     return;
   } catch (error) {
